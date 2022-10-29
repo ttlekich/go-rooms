9d29c564f99473ced78b51fb87d736f123384eec
ttlekich <ttlekich@gmail.com>
2022-10-28 22:19:09 -0500

    added mutexes for concurrent map - reduced time from ns to ms..but still seems ok

--- main.go
+++ main.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 )
 
+var mutex = &sync.RWMutex{}
+
 func main() {
 	r := gin.Default()
+	// this may cause a
+	// fatal error: concurrent map iteration and map write
 	var hubs = make(map[*Hub]bool)
 
-	fmt.Println(hubs)
 	r.GET("/:id", func(c *gin.Context) {
+		fmt.Println(len(hubs))
 		// Adds multi-hub functionality to:
 		// https://yourbasic.org/golang/append-explained/
 		id := c.Param("id")
@@ -19,15 +24,19 @@ func main() {
 		// TODO: How do we destroy hubs when empty?
 		// Can we set up a defer function somewhere that
 		// returns out of the goroutine?
+		mutex.Lock()
 		for hub := range hubs {
 			if hub.Id == id {
 				currentHub = hub
 			}
 		}
+		mutex.Unlock()
 		if currentHub == nil {
 			currentHub = newHub(id)
 			fmt.Println(currentHub)
+			mutex.Lock()
 			hubs[currentHub] = true
+			mutex.Unlock()
 			go currentHub.run()
 		}
 		serveWs(currentHub, c.Writer, c.Request)
